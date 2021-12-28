f22be58fe4c9ec68a39e425a09a2a509acdbd260
Prateek Aher <prat.aher@gmail.com>
2021-12-28 14:55:15 +0530

    Issue with package UI

--- lib/UI/Main/Home/CannotAcceptPackage/issue_with_package.dart
+++ lib/UI/Main/Home/CannotAcceptPackage/issue_with_package.dart
@@ -1,14 +1,104 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class IssueWithPackage extends StatelessWidget {
+import 'package:delivery/CommonWidget/CommonWidget.dart';
+import 'package:delivery/Utils/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+
+class IssueWithPackage extends StatefulWidget {
   const IssueWithPackage({Key? key}) : super(key: key);
+
+  @override
+  State<IssueWithPackage> createState() => _IssueWithPackageState();
+}
+
+class _IssueWithPackageState extends State<IssueWithPackage> {
+  File? xImage;
+
+  @override
+  void initState() {
+    pickImage();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Issue with Package'),
+        title: Text('Confirm'),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            sbh(32),
+            Text(
+              'Issue with Package',
+              style: TextStyle(
+                  fontSize: 26,
+                  fontFamily: 'WorkSans',
+                  fontWeight: FontWeight.w500,
+                  color: BLACK1),
+            ),
+            sbh(16),
+            Text(
+              'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+            sbh(16),
+            if (xImage != null)
+              Container(
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.grey)),
+                  child: Image.file(xImage!))
+            else
+              InkWell(
+                onTap: pickImage,
+                child: Ink(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(8)),
+                    child: Icon(Icons.camera)),
+              ),
+            sbh(24),
+            ElevatedButton(
+              onPressed: xImage == null ? null : () {},
+              child: Text(
+                'Send Picture',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontFamily: 'WorkSans',
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 51, vertical: 16)),
+            ),
+            sbh(48),
+            goBackButton(context),
+          ],
+        ),
       ),
     );
+  }
+
+  Future<dynamic> pickImage() async {
+    try {
+      final image = await ImagePicker().pickImage(
+          source: ImageSource.camera, preferredCameraDevice: CameraDevice.rear);
+      if (image == null) return;
+      final capturedImage = File(image.path);
+      setState(() => this.xImage = capturedImage);
+    } on PlatformException catch (e) {
+      print(e.message);
+    }
   }
 }
