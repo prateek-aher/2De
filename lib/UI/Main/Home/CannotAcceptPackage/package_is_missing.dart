422ee756686761117e62d83b57b32435b89ced0c
Prateek Aher <prat.aher@gmail.com>
2021-12-28 15:00:27 +0530

    Package is Missing UI

--- lib/UI/Main/Home/CannotAcceptPackage/package_is_missing.dart
+++ lib/UI/Main/Home/CannotAcceptPackage/package_is_missing.dart
@@ -1,3 +1,5 @@
+import 'package:delivery/CommonWidget/CommonWidget.dart';
+import 'package:delivery/Utils/colors.dart';
 import 'package:flutter/material.dart';
 
 class PackageIsMissing extends StatelessWidget {
@@ -7,7 +9,46 @@ class PackageIsMissing extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Package is Missing'),
+        title: Text('Confirm'),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            sbh(32),
+            Text(
+              'Package is Missing?',
+              style: TextStyle(
+                  fontSize: 26,
+                  fontFamily: 'WorkSans',
+                  fontWeight: FontWeight.w500,
+                  color: BLACK1),
+            ),
+            sbh(16),
+            Text(
+              'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+            sbh(32),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                'Package Missing',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontFamily: 'WorkSans',
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 51, vertical: 16)),
+            ),
+            sbh(48),
+            goBackButton(context),
+          ],
+        ),
       ),
     );
   }
