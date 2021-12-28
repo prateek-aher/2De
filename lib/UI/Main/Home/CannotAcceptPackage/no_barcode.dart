84a13e8e2da12cecaea3b8b6f413f62bd5c03084
Prateek Aher <prat.aher@gmail.com>
2021-12-28 15:17:46 +0530

    No Barcode UI

--- lib/UI/Main/Home/CannotAcceptPackage/no_barcode.dart
+++ lib/UI/Main/Home/CannotAcceptPackage/no_barcode.dart
@@ -1,13 +1,86 @@
+import 'package:delivery/CommonWidget/CommonWidget.dart';
+import 'package:delivery/Utils/colors.dart';
 import 'package:flutter/material.dart';
 
-class NoBarcode extends StatelessWidget {
+class NoBarcode extends StatefulWidget {
   const NoBarcode({Key? key}) : super(key: key);
+
+  @override
+  State<NoBarcode> createState() => _NoBarcodeState();
+}
+
+class _NoBarcodeState extends State<NoBarcode> {
+  String code = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('No Barcode/ ID'),
+        title: Text('Confirm'),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            sbh(32),
+            Text(
+              'No Barcode or ID',
+              style: TextStyle(
+                  fontSize: 26,
+                  fontFamily: 'WorkSans',
+                  fontWeight: FontWeight.w500,
+                  color: BLACK1),
+            ),
+            sbh(16),
+            Text(
+              'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+            sbh(32),
+            if (code.isNotEmpty) ...[
+              Container(
+                  height: MediaQuery.of(context).size.height * 0.1,
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Center(
+                      child: Text(
+                    'code',
+                    style: TextStyle(
+                        fontFamily: 'WorkSans',
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20,
+                        color: Theme.of(context).primaryColor),
+                  ))),
+              sbh(24)
+            ],
+            ElevatedButton(
+              onPressed: () {
+                if (code.isEmpty) {
+                  // TODO: Fetch code
+                } else {
+                  // TODO: submit with new code
+                }
+              },
+              child: Text(
+                code.isEmpty ? 'Next' : 'Finish',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontFamily: 'WorkSans',
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 51, vertical: 16)),
+            ),
+            sbh(48),
+            goBackButton(context),
+          ],
+        ),
       ),
     );
   }
